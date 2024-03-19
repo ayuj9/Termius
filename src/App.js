b2993e46e0d414b81d93ab04598496da55fa1384
Ayushi Jain <ayuj5571@gmail.com>
2024-03-20 00:13:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from "@mui/material";
+import "./App.css";
+import { SideBar } from "./components/LeftBar";
+import { MiddleBar } from "./components/MiddleBar";
+import { RightBar } from "./components/RightBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid container bgcolor="pink" direction="row" height="100vh">
+        <Grid item lg={1.5} md={2} sm={1} xs={2}>
+          <SideBar />
+        </Grid>
+        <Grid item lg={8} md={8} sm={8} xs={8} height="100vh">
+          <MiddleBar />
+        </Grid>
+        <Grid item lg={2.5} md={1} sm={3} xs={2} height="100vh">
+          <RightBar />
+        </Grid>
+      </Grid>
+      {/* </Grid> */}
+    </>
   );
 }
 
